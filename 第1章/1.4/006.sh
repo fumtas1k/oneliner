64bd812912573139eb8d0ad32e82bc0714ef7d3a
fumtas1k <fumta01a7@gmail.com>
2022-12-07 22:58:24 +0900

    [add]別解追加

--- 第1章/1.4/006.sh
+++ 第1章/1.4/006.sh
@@ -4,3 +4,4 @@
 seq 5 -1 1 | awk '{for(i=0;i<$1;i++){printf " "};{print "x"}}'
 printf "%*s\n" 5 x 4 x 3 x 2 x 1 x
 seq 5 -1 1 | awk '{print $1 " x"}' | xargs printf '%*s\n'
+seq 5 -1 1 | awk '{print 10^$1"x"}' | tr -d 1 | tr 0 ' '
