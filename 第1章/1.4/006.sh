f392fb2b16d20570d10b95ffb403c21829de2c55
fumtas1k <fumta01a7@gmail.com>
2022-12-07 22:16:49 +0900

    [add]解法追加

--- 第1章/1.4/006.sh
+++ 第1章/1.4/006.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # 端末に模様を描く
 
-seq 5 | awk '{for(i=0;i<5-$1;i++){printf " "};{print "x"}}'
+seq 5 -1 1 | awk '{for(i=0;i<$1;i++){printf " "};{print "x"}}'
+printf "%*s\n" 5 x 4 x 3 x 2 x 1 x
+seq 5 -1 1 | awk '{print $1 " x"}' | xargs printf '%*s\n'
