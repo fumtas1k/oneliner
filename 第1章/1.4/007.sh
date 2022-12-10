39c897454bbb3730252d69e5b8869ecace241fd6
fumtas1k <fumta01a7@gmail.com>
2022-12-10 16:10:34 +0900

    [add]007追加

---
+++ 第1章/1.4/007.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# brew install num-utils # numsumのインストール
+
+cat qdata/7/kakeibo.txt | awk '{tax=($1 < "20191001" || $2~/^\*/ ? 1.08 : 1.1);print $0, tax}' | awk '{print int($3*$4)}' | numsum
