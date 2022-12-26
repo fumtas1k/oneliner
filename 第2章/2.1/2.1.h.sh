dcdccb5507f3a885e1f5529ec19a582898201055
fumtas1k <fumta01a7@gmail.com>
2022-12-26 23:27:41 +0900

    [change]テストコマンド変更

--- 第2章/2.1/2.1.h.sh
+++ 第2章/2.1/2.1.h.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # 条件分岐
+# []の中は、1スペースずつ必要
 
-a=0
-if grep '[02468]$' <<< $a; then echo 偶数; elif grep '[13579]$' <<< $a; then echo 奇数; else echo その他; fi
-echo $a | awk '/[0-9]$/{print $1%2 ? "奇数" : "偶数"; exit}{print "その他"}'
-bash -c "grep '[02468]' <<< $a && echo 偶数" || bash -c "grep '[13579]' <<< $a && echo 奇数" || echo その他
+if grep -e '[02468]$' <<< $1; then echo 偶数; elif grep -e '[13579]$' <<< $1; then echo 奇数; else echo その他; fi
+echo $1 | awk '/[0-9]$/{print $1%2 ? "奇数" : "偶数"; exit}{print "その他"}'
+bash -c "grep '[02468]' <<< $1 && echo 偶数" || bash -c "grep '[13579]' <<< $1 && echo 奇数" || echo その他
 
-test 10 -gt "$a"
-echo $?
+[ 10 -gt $1 ]; echo $?
 b="Yes we can!"
-test "$b" = "No we cannot!"
-echo $?
-test -e /etc/passwd; echo $?
+[ "$b" = "No we cannot!" ]; echo $?
+[ -e /etc/passwd ]; echo $?
